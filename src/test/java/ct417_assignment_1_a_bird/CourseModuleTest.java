f41b259052c0366d6acec80b7ad4dfd12904d605
Anthony Bird <a.bird6@universityofgalway.ie>
2022-10-12 11:05:12 +0100

    Changed test imports

--- src/test/java/ct417_assignment_1_a_bird/CourseModuleTest.java
+++ src/test/java/ct417_assignment_1_a_bird/CourseModuleTest.java
@@ -5,17 +5,21 @@
 
 package ct417_assignment_1_a_bird;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+// import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue;
+// import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.LocalDate;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
 
 class CourseModuleTest {
 
@@ -25,7 +29,7 @@ class CourseModuleTest {
 	Module sampleModule;
 	
 	
-	@BeforeEach
+	@Before
 	void setup() {
 		modules = new ArrayList<Module>();
 		course = new CourseProgramme("ECE", modules, new LocalDate(2023, 9, 12), new LocalDate(2024, 5, 6));
@@ -36,22 +40,19 @@ class CourseModuleTest {
 	// ====================== Accessor Method Testing ======================
 	
 	@Test
-	@DisplayName("Test accesor for modules")
 	void testGetModules() {
-		assertEquals(modules, course.getModules(), "CourseProgramme has list of modules defined in setup()");
+		assertEquals("CourseProgramme has list of modules defined in setup()", modules, course.getModules());
 	}
 	
 	// ====================== Mutator Method Testing ======================
 	
 	@Test
-	@DisplayName("Test mutator for adding modules")
 	void testAddModule() {
 		course.addModule(sampleModule);
 		assertTrue("Module list should now contain sample Module", modules.contains(sampleModule));
 	}
 	
 	@Test
-	@DisplayName("Duplicate entry test")
 	void testDuplicateEntry() {
 		course.addModule(sampleModule);
 		course.addModule(sampleModule);
@@ -60,12 +61,11 @@ class CourseModuleTest {
 			if( m== sampleModule )
 				count++;
 		}
-		assertEquals(1, count, "count should be 1 to show no duplicate entries");
+		assertEquals("count should be 1 to show no duplicate entries", 1, count);
 	}
 
 	
 	@Test
-	@DisplayName("Test mutator for removing modules")
 	void testRemoveModule() {
 		course.addModule(sampleModule);
 		course.removeModule(sampleModule);
